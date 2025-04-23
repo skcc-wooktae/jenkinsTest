5453b15f63606296395125a0bf15cf9625263700
hp <hp@host.docker.internal>
2025-04-23 17:07:52 +0900

    first commit

---
+++ src/main/java/myjenkins1/firstproject/Myjenkins1Application.java
@@ -0,0 +1,13 @@
+package myjenkins1.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myjenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myjenkins1Application.class, args);
+	}
+
+}
---
+++ src/test/java/myjenkins1/firstproject/Myjenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package myjenkins1.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myjenkins1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
